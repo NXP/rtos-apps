9783bd174274130f71ec060aba69ab20bb9cfd57
Rui Sousa <rui.sousa@nxp.com>
2025-05-19 16:58:08 +0200

    log: Avoid namespace conflicts
    Avoid namespace conflicts by renaming short macros:
    - Rename log() to rtos_apps_log()
    - Rename log_raw() to rtos_apps_log_raw()
    
    Signed-off-by: Rui Sousa <rui.sousa@nxp.com>

--- rtos_apps/log.h
+++ rtos_apps/log.h
@@ -48,29 +48,33 @@ static inline void rtos_apps_log_timestamp_update(uint64_t timestamp)
  *
  *  Usage:
  *
- *      log(INFO, "---\n");
+ *      rtos_apps_log(INFO, "---\n");
  *  or
- * 	    log_info("---\n");
+ *      log_info("---\n");
  */
-#define log(LEVEL, format, ...)                                                                                        \
+#define rtos_apps_log(LEVEL, format, ...)                                                                              \
     do {                                                                                                               \
         if (rtos_apps_log_level_config >= LOG_##LEVEL)                                                                 \
             rtos_printf(RTOS_APPS_LOG_FMT format "\r", #LEVEL, RTOS_APPS_LOG_ARGS, ##__VA_ARGS__);                     \
     } while (0)
 
-#define log_crit(...)  log(CRIT, __VA_ARGS__)
-#define log_err(...)   log(ERR, __VA_ARGS__)
-#define log_warn(...)  log(WARN, __VA_ARGS__)
-#define log_info(...)  log(INFO, __VA_ARGS__)
-#define log_debug(...) log(DEBUG, __VA_ARGS__)
+#define log_crit(...)  rtos_apps_log(CRIT, __VA_ARGS__)
+#define log_err(...)   rtos_apps_log(ERR, __VA_ARGS__)
+#define log_warn(...)  rtos_apps_log(WARN, __VA_ARGS__)
+#define log_info(...)  rtos_apps_log(INFO, __VA_ARGS__)
+#define log_debug(...) rtos_apps_log(DEBUG, __VA_ARGS__)
 
-#define log_raw(LEVEL, ...)                                                                                            \
+#define rtos_apps_log_raw(LEVEL, ...)                                                                                  \
     do {                                                                                                               \
         if (rtos_apps_log_level_config >= LOG_##LEVEL)                                                                 \
             rtos_printf(__VA_ARGS__);                                                                                  \
     } while (0)
 
-#define log_raw_info(...) log_raw(INFO, __VA_ARGS__)
+#define log_raw_crit(...)  rtos_apps_log_raw(CRIT, __VA_ARGS__)
+#define log_raw_err(...)   rtos_apps_log_raw(ERR, __VA_ARGS__)
+#define log_raw_warn(...)  rtos_apps_log_raw(WARN, __VA_ARGS__)
+#define log_raw_info(...)  rtos_apps_log_raw(INFO, __VA_ARGS__)
+#define log_raw_debug(...) rtos_apps_log_raw(DEBUG, __VA_ARGS__)
 
 /** Set current log level configuration */
 static inline void rtos_apps_log_level_config_set(rtos_apps_log_level_t rtos_apps_log_level)
